67f5956e8a7403bc22c43c83a51655d7eeb6cd89
Vinay Singh <vinaysingh8866@gmail.com>
2022-06-06 11:49:40 -0400

    Updates

---
+++ _next/static/E4MYQV2CWe0tZBTECg-fo/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-7f5cf25e8826593e1cd1.js"],"/_error":["static/chunks/pages/_error-077790e38fbfe3891eb2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
